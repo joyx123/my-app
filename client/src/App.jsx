b5d753d99f90bca6772ba209a6d1f90dfe7c8b5b
joy <joyxia123@outlook.com>
2025-06-16 10:16:32 -0500

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>{users.map(u => <li key={u.id}>{u.name}</li>)}</ul>
+    </div>
+  );
+}
+
+export default App;
